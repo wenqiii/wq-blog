d4c580931453d91b45d11592be9b6152cd2d1913
程文琪 <chengwenqi@Bowens-MacBook-Pro.local>
2020-08-25 15:16:01 +0800

    build floder

---
+++ precache-manifest.e4f83dec667fc5f7992fd8569ec1eaba.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efb5247bbb7856a8bbd67402386715d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e1f4341e42dbd6802528",
+    "url": "./static/css/2.6ed879b5.chunk.css"
+  },
+  {
+    "revision": "2cbfc73825104f0bf6d4",
+    "url": "./static/css/main.3c9d8e5e.chunk.css"
+  },
+  {
+    "revision": "e1f4341e42dbd6802528",
+    "url": "./static/js/2.c731a62a.chunk.js"
+  },
+  {
+    "revision": "3780c1cb5da1f1b68e5488f5a00d0317",
+    "url": "./static/js/2.c731a62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbfc73825104f0bf6d4",
+    "url": "./static/js/main.f3f25434.chunk.js"
+  },
+  {
+    "revision": "e21397ecf58f77891282",
+    "url": "./static/js/runtime-main.8ee73f49.js"
+  },
+  {
+    "revision": "2204d206370947aa3158bded1c2924ab",
+    "url": "./static/media/1.2204d206.jpg"
+  },
+  {
+    "revision": "a8a01fd4fa89c7ed71a48651d728b846",
+    "url": "./static/media/bloom.a8a01fd4.jpg"
+  },
+  {
+    "revision": "d5db7805f3efd881863b741e2e7c274b",
+    "url": "./static/media/blue-bell.d5db7805.jpg"
+  },
+  {
+    "revision": "3dceacae08b9b20ce63277ce2c48a091",
+    "url": "./static/media/building.3dceacae.jpg"
+  },
+  {
+    "revision": "7a4567b5983cd6d0d3e6999a4e859d3c",
+    "url": "./static/media/forest.7a4567b5.png"
+  },
+  {
+    "revision": "fc7dfb689c07b6ed91f67de0b5a09346",
+    "url": "./static/media/keyboard.fc7dfb68.jpg"
+  },
+  {
+    "revision": "ba757c66aa059b03ddee97ad65d89491",
+    "url": "./static/media/market.ba757c66.jpg"
+  }
+]);
